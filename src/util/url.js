d044c3aa4f7919956a4a70797a6feda00c528ccd
blackc <blacks.chaiwat@gmail.com>
2026-04-06 18:35:31 +0700

    fix url path

--- src/util/url.js
+++ src/util/url.js
@@ -1,22 +1,24 @@
 export const URL_ENDPOINT = "https://developers.promptkai.com";
 
+const path = 'commercedev' // commercedev, commerce
+
 const URL = {
-    LOGIN: `${URL_ENDPOINT}/commercedev/api/admin/loginwithuser`,
-    PROFILE: `${URL_ENDPOINT}/commercedev/api/admin/getinfo`,
+    LOGIN: `${URL_ENDPOINT}/${path}/api/admin/loginwithuser`,
+    PROFILE: `${URL_ENDPOINT}/${path}/api/admin/getinfo`,
 
-    DASHBOARD: `${URL_ENDPOINT}/commercedev/api/cms/dashboard/commerce`,
-    ORDER_LIST: `${URL_ENDPOINT}/commercedev/api/cms/order/list`,
-    ORDER_DETAIL: `${URL_ENDPOINT}/commercedev/api/cms/order/detail`,
-    ORDER_MASTER: `${URL_ENDPOINT}/commercedev/api/cms/order/masterdata`,
+    DASHBOARD: `${URL_ENDPOINT}/${path}/api/cms/dashboard/commerce`,
+    ORDER_LIST: `${URL_ENDPOINT}/${path}/api/cms/order/list`,
+    ORDER_DETAIL: `${URL_ENDPOINT}/${path}/api/cms/order/detail`,
+    ORDER_MASTER: `${URL_ENDPOINT}/${path}/api/cms/order/masterdata`,
 
-    PRODUCT_LIST: `${URL_ENDPOINT}/commercedev/api/cms/zortproduct/list`,
-    PRODUCT_MASTER: `${URL_ENDPOINT}/commercedev/api/cms/product/masterdata`,
+    PRODUCT_LIST: `${URL_ENDPOINT}/${path}/api/cms/zortproduct/list`,
+    PRODUCT_MASTER: `${URL_ENDPOINT}/${path}/api/cms/product/masterdata`,
 
-    CUSTOMER_LIST: `${URL_ENDPOINT}/commercedev/api/cms/customer/list`,
+    CUSTOMER_LIST: `${URL_ENDPOINT}/${path}/api/cms/customer/list`,
 
-    SKU_LIST: `${URL_ENDPOINT}/commercedev/api/product/list`,
-    SKU_ADD: `${URL_ENDPOINT}/commercedev/api/product/add`,
-    SKU_EXPORT: `${URL_ENDPOINT}/commercedev/api/product/export`,
+    SKU_LIST: `${URL_ENDPOINT}/${path}/api/product/list`,
+    SKU_ADD: `${URL_ENDPOINT}/${path}/api/product/add`,
+    SKU_EXPORT: `${URL_ENDPOINT}/${path}/api/product/export`,
 };
 
 export default URL;
